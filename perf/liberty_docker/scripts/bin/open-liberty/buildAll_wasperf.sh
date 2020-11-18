f0989b265c3c5bf93d4bd392454356214a633eae
Emmanuel Ramos <emmanuel.ramos2@ibm.com>
2020-11-18 13:08:33 -0400

    Added SUFP scenarios for Websphere Liberty Docker Image testing

---
+++ perf/liberty_docker/scripts/bin/open-liberty/buildAll_wasperf.sh
@@ -0,0 +1,177 @@
+#!/usr/bin/env bash
+
+# Finds the latest Open Liberty development driver and builds the Docker images based on its install images.
+# If this is a Travis build on master, images are tagged openliberty/daily:<image> and pushed to Docker Hub
+
+set -e
+cd $(dirname $0)
+readonly usage="Usage: buildAll.sh"
+
+readonly NIGHTLY_URL="https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"
+
+readonly BUILD=${1}
+readonly BASE_TAG=${2}
+
+if [[ ${BASE_TAG} == *java8-openj9-ubi* ]]
+then
+  FILE_NAME="ubi.adoptopenjdk8"
+elif [[ ${BASE_TAG} == *java11-openj9-ubi* ]]
+then
+  FILE_NAME="ubi.adoptopenjdk11"
+elif [[ ${BASE_TAG} == *java8-ibmjava-ubi* ]]
+then
+  FILE_NAME="ubi.ibmjava8"
+elif [[ ${BASE_TAG} == *java8-openj9* ]]
+then
+  FILE_NAME="ubuntu.adoptopenjdk8"
+else
+  echo "ERROR"
+  exit
+fi
+
+main () {
+    ## build ibmjava base image
+    rm -rf java
+    build_ubi_base
+    ## Fetch list of urls
+    
+    local urls=$(fetch_liberty_urls)
+    urls=($urls)
+
+    echo $urls
+    ## Loop through list in reverse order, break on valid build
+    local args
+    found=false
+    for (( i=${#urls[@]}-1 ; i>=0 ; i-- )); do
+        echo "****** Parsing url: ${urls[i]}"
+        args=$(parse_build_url "${urls[i]}")
+        # if args is not empty string build was successful
+        if [[ ! -z "${args}" ]] && [[ ${BUILD} == "latest" ]]
+        then
+            args=($args) #convert space seperate string to arr
+            found=true
+            break
+        fi
+        echo ${urls[i]}
+        if [[ ! -z "${args}" ]] 
+        then
+            args=($args) #convert space seperate string to arr
+            if [[ ${args[1]} == *$BUILD* ]] 
+            then
+              found=true
+              break
+            fi
+        fi
+    done
+
+    if [[ found == false ]]
+    then 
+      exit 1
+    fi
+
+    ## Set arguments for the build script
+    local fullImageUrl="${args[0]}"
+    local buildLabel="${args[1]}"
+    local version="${args[2]}"
+
+    if [[ -z "${fullImageUrl}" || -z "${buildLabel}" || -z "${version}" ]]; then
+        echo "ERROR: Could not find a valid build with all needed install images available"
+        exit 1
+    fi
+
+    echo "****** Found latest build"
+    printf "URL: %s \nLabel: %s \nVersion: %s\n" "${fullImageUrl}" "${buildLabel}" "${version}"
+    #cd ci.docker
+    echo "****** Starting daily build from $(pwd)..."
+
+    ./build-wasperf.sh --version="${version}" --buildLabel="${buildLabel}" --fullDownloadUrl="${fullImageUrl}" --baseTag="${BASE_TAG}" --fileName="${FILE_NAME}"
+    docker tag openliberty/daily:full-${BASE_TAG}  openliberty/daily:latest
+}
+## builds the ibmjava base for ./build.sh script
+build_ubi_base() {
+  docker pull registry.access.redhat.com/ubi8/ubi
+  ## pull Dockerfile from ibmjava
+  mkdir java
+  wget https://raw.githubusercontent.com/ibmruntimes/ci.docker/master/ibmjava/8/jre/ubi/Dockerfile -O java/Dockerfile
+
+  ## replace references to user 1001 as we need to build as root
+  sed -i.bak '/useradd -u 1001*/d' ./java/Dockerfile && sed -i.bak '/USER 1001/d' ./java/Dockerfile && rm java/Dockerfile.bak
+  docker build -t ibmjava:8-ubi java
+}
+## @returns a list of strings representing the nightly liberty builds, old to newest
+fetch_liberty_urls() {
+    ## Generate Download URL, SHA, build label
+    local buildList=$(curl -s "${NIGHTLY_URL}" | grep folder.gif)
+    local buildUrls=()
+
+    while read -r currentLine; do
+        if is_build_link "${currentLine}"; then
+            buildUrls+=("${NIGHTLY_URL}${BASH_REMATCH[1]}")
+            echo ${NIGHTLY_URL}${BASH_REMATCH[1]}
+        fi
+    done <<< "${buildList}"
+
+     
+    ## this is equivalent to returning a space seperated str of urls (iterable)
+    echo "${buildUrls[@]}"
+}
+## @returns space seperated string if successfully, empty string on failure
+parse_build_url() {
+    local buildUrl="$1"
+    declare fullImageFile buildLabel version output
+    ## loop through items listed under build and format arguments
+    fileList=$(curl -s "${buildUrl}/" | egrep "openliberty|info.json")
+    while read -r current_file; do
+        ## if the tests did not pass return empty output string
+        if is_info_file "${current_file}" && ! is_build_success "${current_file}" "${buildUrl}"; then
+            break
+        elif is_full_file "${current_file}"; then
+            fullImageUrl="${buildUrl}/${BASH_REMATCH[1]}${BASH_REMATCH[2]}-${BASH_REMATCH[3]}.zip"
+            version="${BASH_REMATCH[2]}"
+            buildLabel="${BASH_REMATCH[3]}"
+            output="${fullImageUrl} ${buildLabel} ${version}"
+        fi
+    done <<< "${fileList}"
+    ## return the required build arguments for running build script
+    echo "${output}"
+}
+## @returns status code of the test
+is_build_success() {
+    local info_file="$1"; shift
+    local current_url="$1"
+    # variables for comparison in return
+    declare testsPass testsRun
+    # fetch jsonfile
+    local json=$(curl -s "${current_url}/info.json" | grep test)
+    while read -r jsonLine; do
+        if [[ $jsonLine =~ \"test_passed\":[[:blank:]]\"([0-9]+)\" ]]; then
+          testsPass=${BASH_REMATCH[1]}
+        elif [[ $jsonLine =~ \"total_tests\":[[:blank:]]\"([0-9]+)\" ]]; then
+          testsRun=${BASH_REMATCH[1]}
+        fi
+    done <<< "${json}"
+    ## return status
+    [[ "${testsRun}" -ne 0 && "${testsPass}" -eq "${testsRun}" ]]
+}
+
+### REGEX conditionals
+## Usage for all: `if is_condition <param>; then ...`
+is_build_link() {
+    local link_tag="$1"
+    [[ $link_tag =~ .*([0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{4}).* ]]
+}
+is_kernel_file() {
+    local str="$1"
+    [[ $str =~ \>(openliberty-)([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)-(.*)\.zip ]]
+}
+is_full_file() {
+    local str="$1"
+
+    [[ $str =~ \>(openliberty-all-)([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)-(.*)\.zip ]]
+}
+is_info_file() {
+    local str="$1"
+
+    [[ $str =~ info.json ]]
+}
+main
---
+++ perf/liberty_docker/scripts/bin/websphere-liberty/build-daily-images-wasperf.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env bash
+
+set -Eeo pipefail
+
+readonly REPO="websphere-liberty-daily"
+readonly VERSION="20.0.0.6"
+readonly DOWNLOAD_URL="https://libfsfe04.hursley.ibm.com/liberty/dev/Xo/release2"
+#readonly DOWNLOAD_URL="https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"
+readonly USERNAME="$1"
+readonly PASSWORD="$2"
+
+readonly BUILD="${3}"
+readonly BASE_TAG="${4}"
+
+echo $USERNAME
+echo $PASSWORD
+#echo $BUILD
+
+if [[ ${BASE_TAG} == *java8-openj9-ubi* ]]
+then
+  FILE_NAME="ubi.adoptopenjdk8"
+elif [[ ${BASE_TAG} == *java11-openj9-ubi* ]]
+then
+  FILE_NAME="ubi.adoptopenjdk11"
+elif [[ ${BASE_TAG} == *java8-ibmjava-ubi* ]]
+then
+  FILE_NAME="ubi.ibmjava8"
+elif [[ ${BASE_TAG} == *java8-ibmjava* ]]
+then
+  FILE_NAME="ubuntu.ibmjava8"
+else
+  echo "ERROR"
+  exit
+fi
+
+main() {
+  if [[ -z "${USERNAME}" || -z "${PASSWORD}" ]]; then
+    echo "****** Error: w3 authentication not set, exiting..."
+    exit 1
+  fi
+
+  echo "****** Querying for last successful build..."
+  local full_build_label=$(get_full_build_label)
+  
+  if [[ $full_build_label == "" ]]
+  then
+    echo "${BUILD} not found"
+    exit
+  fi
+
+  echo "****** Full Build Label: ${full_build_label}"
+  ## Removes everything up to the % symbol
+  local short_build_label=$(format_short_build_label "${full_build_label}")
+  local liberty_url=$(format_liberty_url "${full_build_label}")
+  cd ci.docker
+
+  echo "****** Running build script in $(pwd) with:"
+  echo "Build label: ${short_build_label}"
+  echo "Build URL: ${liberty_url}"
+  ../build-wasperf.sh --username="${USERNAME}" --password="${PASSWORD}" --repository="${REPO}" \
+              --version="${VERSION}" --buildLabel="${short_build_label}" --buildUrl="${liberty_url} --baseTag="${BASE_TAG} --fileName="${FILE_NAME}"
+  #docker tag websphere-liberty-daily:full-${BASE_TAG} websphere-liberty:full
+  docker tag websphere-liberty-daily:full-${BASE_TAG} websphereliberty/daily:latest
+
+}
+## @returns the last good build's label as a string
+get_full_build_label() {
+  ## last.good.build.label is a text file containing the last good build label for convenience
+  ## --anyauth checks what auth is required and uses it's safest protocol automatically
+  if [[ ${BUILD} == "latest" ]] 
+  then
+    echo $(curl -k -u "${USERNAME}:${PASSWORD}" "${DOWNLOAD_URL}/last.good.build.label")
+  else
+    while IFS= read -r line
+    do
+      printf '%s\n' "$line"
+    done < <(curl -L -k -u "${USERNAME}:${PASSWORD}" "${DOWNLOAD_URL}" | sed -n -r "s@.*(${BUILD}-.*?)/</a>.*@\1@p" )
+ fi
+  
+}
+## @returns the build label without the extra id string at the end, as used to label docker image
+format_short_build_label() {
+  local full_build_label="$1"
+  echo "${full_build_label%-_*}"
+}
+## @returns the URL to full.zip file that will be used as an arg during docker build
+format_liberty_url() {
+  local full_build_label="$1"
+  local build_label=$(format_short_build_label "${full_build_label}")
+  echo "${DOWNLOAD_URL}/${full_build_label}/wlp-${build_label}.zip"
+}
+
+is_build_link() {
+    local link_tag="$1"
+    [[ $link_tag =~ .*([0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{4}).* ]]
+}
+
+
+main $@
+########## Not currently used ##########
+# The below is necessary if in the future we decide to build kernel daily images
+# alongside full, but as of now we just take the full zip for daily.
+# kernel url: ${DOWNLOAD_URL}/${full_build_label}/fe/${build_name}/linux/zipper/externals/installables/
+
+## @returns the selected build's name as a string
+## downloads build directory structure and traverses it to get name of directory labelled after build name
+get_build_name() {
+  local build_path="$1"
+  local url="https://${build_path}"
+  ## get build dirs
+  wget --no-parent --spider -r -l1 --no-check-certificate --user="${USERNAME}" --password="${PASSWORD}" "${url}" > /dev/null 2>&1
+  ## traverse and print the directory named after build
+  local name=$(ls ${build_path})
+  rm -rf libfsfe01.hursley.ibm.com
+  echo "${name}"
+}
