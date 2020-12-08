980ee6d3adeedf36e117561d091e2d229b34609f
Emmanuel Ramos <emmanuel.ramos2@ibm.com>
2020-12-08 13:52:28 -0400

    Update configure_liberty_docker_test.sh

--- perf/liberty_docker/scripts/bin/configure_liberty_docker_test.sh
+++ perf/liberty_docker/scripts/bin/configure_liberty_docker_test.sh
@@ -150,10 +150,11 @@ downloadDepencies()
 			    echo "Use GIT_TOKEN to authenticate - $GIT_TOKEN"
 				echo $GIT_TOKEN
 				echo  '$GIT_TOKEN' | base64
-				CURL_CMD="curl -OLk -H 'Authorization: token $GIT_TOKEN' ${APP_URL}"
+				CURL_CMD="curl -OLk -H 'Authorization: token $GIT_TOKEN' \"${APP_URL}\""
 				#CURL_CMD="eval \"$CURL_CMD\""
 				#echo $CURL_CMD >> $LIBERTY_DEP_CACHE_LOCATION/down.txt
 				echo "This is the command ${CURL_CMD}"
+				curl --help
 				ls $LIBERTY_DEP_CACHE_LOCATION
 			fi
 			 
