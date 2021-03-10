3d73675af7190d4d35b0327721abed7399c4c5ab
Emmanuel Ramos <emmanuel.ramos2@ibm.com>
2021-03-10 11:09:31 -0500

    Update configure_liberty.sh

--- perf/liberty/scripts/bin/configure_liberty.sh
+++ perf/liberty/scripts/bin/configure_liberty.sh
@@ -269,6 +269,11 @@ echoAndRunCmd "mkdir -p ${DEST} ${LIBERTY_DEP_CACHE_LOCATION}"
 # CHECK IF LATEST BUILD IS DESIRED
 LIBERTYFS_URL=https://${LIBERTY_BUILD_REPO}/liberty/dev/${LIBERTY_BUILD_STREAM}
 echo "LIBERTYFS_URL=${LIBERTYFS_URL}"
+# Check if the Liberty build level is customized
+if [[ "${LIBERTY_BUILD_LEVEL}" == "customized" ]]; then
+	LIBERTY_BUILD_LEVEL=${CUSTOMIZED_LIBERTY_BUILD_LEVEL}
+fi
+
 if [[ "${LIBERTY_BUILD_LEVEL}" == "latest" ]]; then
 	getLibertyLatestBuildLabel
 	# Get the build from LIBERTYFS_BUILD_URL so we can use it for WL_ZIP - https://libertyfs.hursley.ibm.com/liberty/dev/Xo/release/cl210220210125-1100-_GAq8QF70Eeu-m6gcHvZdzA -> cl210220210125-1100
