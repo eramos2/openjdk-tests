0433c129a5f3ffd1cb9137f84082fe415f5aa18c
Emmanuel Ramos <emmanuel.ramos2@ibm.com>
2021-03-24 11:39:23 -0400

    copy server.env to wlp server dir, for new acmeair scenarios

--- perf/liberty/scripts/sufp/scripts/sufpSetupServers.sh
+++ perf/liberty/scripts/sufp/scripts/sufpSetupServers.sh
@@ -2,7 +2,7 @@
 # Array of all the apps to setup/run
 #apps=(acmeair-micro acmeair-mono cdi-base cdi-fat cdi-one-jar-fat dt7 dt8 huge-ejb javaee7 javaee8 jaxrs-fat jenkins micro-profile-3 no-app no-feat petclinic pingperf spring-1.5.6 spring-2.1.1 tradelite7 tradelite8 webProfile7 webProfile8)
 #apps=(dt7)
-echo $APP
+echo "${APP}"
 sufpScriptDir=$1
 
 echo "about to kill all java procs, unless you stop me in next 10 secs"
@@ -10,11 +10,11 @@ sleep 15
 #killall -9 java
 #pkill -f "/WPA_INST*"
 for jar in ws-launch.jar ws-server.jar; do
-    PS_MATCH=`ps aux | grep java | grep ${curr} | grep $jar | grep javaagent | grep -v grep`
+    PS_MATCH=$(ps aux | grep java | grep "${curr}" | grep $jar | grep javaagent | grep -v grep)
     if [ -n "$PS_MATCH" ]; then
 	  # Echo processes into build log (stdout)
       echo "$PS_MATCH"
-	pids=`ps aux | grep java | grep $PWD | grep $jar | grep javaagent | grep -v grep | awk '{print $2}'`
+	pids=$(ps aux | grep java | grep $PWD | grep $jar | grep javaagent | grep -v grep | awk '{print $2}')
 	for pid_x in $pids
 	do
 	  kill -9 ${pid_x}
@@ -48,9 +48,10 @@ numaargs="numactl --physcpubind ${PHYS_CPU_BIND}"
 
 $numaargs ./bin/server create ${APP}  
 /usr/bin/cp -f ${targDir}/${APP}/server.xml usr/servers/${APP}/
+/usr/bin/cp -f ${targDir}/${APP}/server.env usr/servers/${APP}/
 #Edit server.xml to point to app location
 sed -i "s|\"/sufp/apps|\"${targDir}|g" usr/servers/${APP}/server.xml  
-test=`echo ${targDir}/${APP} | sed -e "s/\// /g"`
+test=$(echo ${targDir}/${APP} | sed -e "s/\// /g")
 if [[ `echo $test | grep spring ` ]] ; then
 	mkdir usr/servers/${APP}/dropins/spring
 	cp ${targDir}/${APP}/*.jar usr/servers/${APP}/dropins/spring
